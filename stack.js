3cf94a0a5929f052a3a5ba5b08ecbd2b45028ae3
Nicole Honl <nicole.m.honl@gmail.com>
2024-04-05 10:38:11 -0500

    finished stack.js

--- stack.js
+++ stack.js
@@ -20,26 +20,44 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let current = this.first;
+      this.first = newNode;
+      this.first.next = current;
+    }
+    this.size++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (!this.first) {
+      throw new Error("No list to remove from");
+    }
+    let current = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
